72eb9de409a4d620ff76edec3980e0db887a5206
iuruoy-shao <youruishao115022@gmail.com>
2024-11-04 21:10:56 -0800

    demo

--- app.py
+++ app.py
@@ -47,11 +47,7 @@ def approx_eq(a, b):
     return abs(a - b) < .0001
 def normalize(values):
     return (values - np.min(values)) / (np.max(values) - np.min(values)) * 255
-def valuemap(constants):
-    i = constants['type']
-    coefficients = constants['coefficients']
-    eq = EulerCauchy(coefficients) if i else ConstantCoefficients(coefficients)
-    eq.constants = constants['constants']
+def valuemap(eq):
     return np.array(list(map(eq.y, np.arange(1,181))))
 def generate_image(rx, gx, bx, ry, gy, by):
     rx = normalize(sum(valuemap(_) for _ in rx))
@@ -67,12 +63,10 @@ def generate_image(rx, gx, bx, ry, gy, by):
     x_values = np.repeat(x_values[:,np.newaxis,:], 180, axis=1)
     y_values = np.repeat(y_values[np.newaxis,:,:], 180, axis=0)
     return ((x_values + y_values) / 2).astype(np.uint8)
-
 def display_solution(eq, constants):
     solution = "y = "
     i = 0
     roots = eq.roots()
-    print(constants, eq.roots())
     for root in roots.keys():
         if root.imag:
             for n_rep in range(0, roots[root], 2):
@@ -94,37 +88,42 @@ def display_solution(eq, constants):
                     case _:
                         solution += f"+ {constants[i]}x^{{{round(root.real, 2)}}} (\ln x)^{n_rep}"
                 i += 1
-    return solution
+    return solution.replace("= +", "=")
 
-n_equations = st.slider("Number of Equations", min_value=1, max_value=5, step=1)
-for i in range(n_equations):
-    expander = st.expander(f"#### Equation {i + 1}")
-    sliders, equation = st.columns(2)
-    with sliders:
-        order = expander.slider("Order of Equation", min_value=2, max_value=5)
-        display_eq = ""
-        for j in range(order + 1, 0, -1):
-            display_eq += "{}" + (f"x^{j-1}" if j-1 > 1 else "x" if j-1 == 1 else "") + "y" + '\'' * (j - 1) + " + "
-        display_eq += "= 0"
-        coefficients = tuple(expander.slider(f"ODE Coefficient {j + 1}", min_value=0.0, max_value=1.0, step=.01) for j in range(order + 1))
-    
-    with equation:
-        expander.write("##### ODE")
-        expander.latex(display_eq.format(*coefficients).replace("+ -", "- ").replace("+ =", "="))
+st.set_page_config(layout="wide")
 
-        expander.write("##### Solution")
-        constants = tuple(expander.slider(f"Solution Coefficient {j + 1}", min_value=0.0, max_value=1.0, value=1.0, step=.01) for j in range(order))
-        eq = EulerCauchy(coefficients)
-        eq.constants = constants
-        expander.latex(display_solution(eq, constants).replace("= +", "="))
+options, image = st.columns(2)
+with options:
+    dependents = ('R', 'G', 'B')
+    independents = ('x', 'y')
+    eqs = {f'{dep}{ind}':(EulerCauchy(np.zeros(3)),) for dep in dependents for ind in independents}
+    for ind in independents:
+        for dep in dependents:
+            expander = st.expander(f"#### {dep}({ind})")
+            with expander:
+                sliders, equation = st.columns(2)
+                with sliders:
+                    order = st.slider("Order of Equation", min_value=2, max_value=5, key=f"{dep}{ind}")
+                    display_eq = ""
+                    for j in range(order + 1, 0, -1):
+                        display_eq += "{}" + (f"x^{j-1}" if j-1 > 1 else "x" if j-1 == 1 else "") + "y" + '\'' * (j - 1) + " + "
+                    display_eq += "= 0"
+                    coefficients = tuple(st.slider(f"ODE Coefficient {j + 1}", 
+                                                        value=.5, min_value=0.0, max_value=1.0, step=.01, key=f"ODE {dep}{ind}{j}") 
+                                                        for j in range(order + 1))
+                
+                with equation:
+                    st.write("##### ODE")
+                    st.latex(display_eq.format(*coefficients).replace("+ -", "- ").replace("+ =", "="))
+                    st.write("##### Solution")
+                    constants = tuple(st.slider(f"Solution Coefficient {j + 1}", 
+                                                    min_value=0.0, max_value=1.0, value=1.0, step=.01, key=f"SOL {dep}{ind}{j}") 
+                                                    for j in range(order))
+                    eq = EulerCauchy(coefficients)
+                    eq.constants = constants
+                    eqs[f'{dep}{ind}'] = (eq,)
+                    st.latex(display_solution(eq, constants))
 
-constants = ([
-    {
-        'type': 1,
-        'coefficients': np.random.rand(3) * 5 - 10,
-        'constants': np.random.rand(3),
-    } for _ in range(5)
-] for _ in range(6))
-
-generated_image = generate_image(*constants)
-plt.imshow(generated_image)
+with image:
+    generated_image = generate_image(*tuple(eqs.values()))
+    st.image(generated_image, use_column_width=True)
